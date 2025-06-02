20a6e8bbd2773a939f50e9609f1f5a52c125ae06
CATALINA-Architecture <guido.averna@icar.cnr.it>
2025-06-02 18:24:49 +0200

    Delete src directory

--- src/Cara_Simulation/TType_Green_Goal.java
+++
@@ -1,8 +0,0 @@
-package Cara_Simulation;
-
-public enum TType_Green_Goal {
-	
-	TGG_Locomotive,
-	TGG_Number_of_train_cars	
-
-}
--- src/Cara_Simulation/TType_Quality_Goal.java
+++
@@ -1,7 +0,0 @@
-package Cara_Simulation;
-
-public enum TType_Quality_Goal {
-	TGQ_Locomotive,
-	TGQ_Panorama,
-	TGQ_Velocity	
-}
