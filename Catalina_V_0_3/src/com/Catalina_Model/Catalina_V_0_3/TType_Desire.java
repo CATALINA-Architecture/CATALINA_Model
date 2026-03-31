97404aa78c7a73b6edd0dff3c0384cf75043b918
CATALINA-Architecture <guido.averna@icar.cnr.it>
2026-03-31 16:09:22 +0200

    Delete Catalina_V_0_3/src/com/Catalina_Model/Catalina_V_0_3 directory

--- Catalina_V_0_3/src/com/Catalina_Model/Catalina_V_0_3/TType_Desire.java
+++
@@ -1,8 +0,0 @@
-package com.Catalina_Model.Catalina_V_0_3;
-
-public enum TType_Desire 
-{
-	Endogenous_Practical_Desire,
-	Exogenous_Epistemic_Desire,
-	Exogenous_Practical_Desire
-}
--- Catalina_V_0_3/src/com/Catalina_Model/Catalina_V_0_3/TType_Temporal_Operator.java
+++
@@ -1,9 +0,0 @@
-package com.Catalina_Model.Catalina_V_0_3;
-
-public enum TType_Temporal_Operator 
-{
-	None,
-	Finally,
-	Globally,
-	Until
-}
